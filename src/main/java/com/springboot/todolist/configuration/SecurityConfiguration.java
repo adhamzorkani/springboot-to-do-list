324516c06aed1bc401b8a15802195dd89896f61b
adhamzorkani <adham.zorkani@aucegypt.edu>
2023-07-25 16:38:42 +0300

    .

--- src/main/java/com/springboot/todolist/configuration/SecurityConfiguration.java
+++ src/main/java/com/springboot/todolist/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.springboot.todolist.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,12 +25,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated();
+        http.csrf().disable().authorizeRequests().antMatchers("/api").permitAll().anyRequest().authenticated();
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
